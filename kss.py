0ee04f5e8555756724449c2ecf41b6133f0b4527
David Corbett <corbett.dav@husky.neu.edu>
2018-04-21 14:29:15 -0400

    Change the font name, version, and legal details

--- kss.py
+++ kss.py
@@ -18,6 +18,7 @@ __all__ = ['augment']
 
 import math
 import os
+import re
 import tempfile
 
 import fontforge
@@ -26,6 +27,105 @@ import psMat
 CLUSTER_Y_SCALE = 0.8
 MAX_EXTRA_ROWS = 3
 INTRA_CLUSTER_GAP = 0.04
+VERSION = '1.000'
+FONT_NAME = 'Khitan Small Stacked'
+COPYRIGHT = '''Copyright (c) 2018, David Corbett (corbett.dav@husky.neu.edu).
+Copyright (c) 2013, Andrew West (www.babelstone.co.uk).'''
+LICENSE = '''Copyright (c) 2018, David Corbett (corbett.dav@husky.neu.edu).
+Copyright (c) 2013, Andrew West (www.babelstone.co.uk),
+with Reserved Font Name BabelStone.
+
+This Font Software is licensed under the SIL Open Font License, Version 1.1.
+This license is copied below, and is also available with a FAQ at:
+http://scripts.sil.org/OFL
+
+
+-----------------------------------------------------------
+SIL OPEN FONT LICENSE Version 1.1 - 26 February 2007
+-----------------------------------------------------------
+
+PREAMBLE
+The goals of the Open Font License (OFL) are to stimulate worldwide
+development of collaborative font projects, to support the font creation
+efforts of academic and linguistic communities, and to provide a free and
+open framework in which fonts may be shared and improved in partnership
+with others.
+
+The OFL allows the licensed fonts to be used, studied, modified and
+redistributed freely as long as they are not sold by themselves. The
+fonts, including any derivative works, can be bundled, embedded,
+redistributed and/or sold with any software provided that any reserved
+names are not used by derivative works. The fonts and derivatives,
+however, cannot be released under any other type of license. The
+requirement for fonts to remain under this license does not apply
+to any document created using the fonts or their derivatives.
+
+DEFINITIONS
+"Font Software" refers to the set of files released by the Copyright
+Holder(s) under this license and clearly marked as such. This may
+include source files, build scripts and documentation.
+
+"Reserved Font Name" refers to any names specified as such after the
+copyright statement(s).
+
+"Original Version" refers to the collection of Font Software components as
+distributed by the Copyright Holder(s).
+
+"Modified Version" refers to any derivative made by adding to, deleting,
+or substituting -- in part or in whole -- any of the components of the
+Original Version, by changing formats or by porting the Font Software to a
+new environment.
+
+"Author" refers to any designer, engineer, programmer, technical
+writer or other person who contributed to the Font Software.
+
+PERMISSION & CONDITIONS
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of the Font Software, to use, study, copy, merge, embed, modify,
+redistribute, and sell modified and unmodified copies of the Font
+Software, subject to the following conditions:
+
+1) Neither the Font Software nor any of its individual components,
+in Original or Modified Versions, may be sold by itself.
+
+2) Original or Modified Versions of the Font Software may be bundled,
+redistributed and/or sold with any software, provided that each copy
+contains the above copyright notice and this license. These can be
+included either as stand-alone text files, human-readable headers or
+in the appropriate machine-readable metadata fields within text or
+binary files as long as those fields can be easily viewed by the user.
+
+3) No Modified Version of the Font Software may use the Reserved Font
+Name(s) unless explicit written permission is granted by the corresponding
+Copyright Holder. This restriction only applies to the primary font name as
+presented to the users.
+
+4) The name(s) of the Copyright Holder(s) or the Author(s) of the Font
+Software shall not be used to promote, endorse or advertise any
+Modified Version, except to acknowledge the contribution(s) of the
+Copyright Holder(s) and the Author(s) or with their explicit written
+permission.
+
+5) The Font Software, modified or unmodified, in part or in whole,
+must be distributed entirely under this license, and must not be
+distributed under any other license. The requirement for fonts to
+remain under this license does not apply to any document created
+using the Font Software.
+
+TERMINATION
+This license becomes null and void if any of the above conditions are
+not met.
+
+DISCLAIMER
+THE FONT SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO ANY WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT
+OF COPYRIGHT, PATENT, TRADEMARK, OR OTHER RIGHT. IN NO EVENT SHALL THE
+COPYRIGHT HOLDER BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+INCLUDING ANY GENERAL, SPECIAL, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL
+DAMAGES, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF THE USE OR INABILITY TO USE THE FONT SOFTWARE OR FROM
+OTHER DEALINGS IN THE FONT SOFTWARE.'''
 
 def cgj(font):
     glyph = font.createChar(0x034F, 'uni034F')
@@ -53,6 +153,20 @@ def add_lookups(font):
         ((b'mark', ((b'DFLT', ('dflt',)),)),))
     font.addLookupSubtable("'mark'", "'mark'-1")
     font.addAnchorClass("'mark'-1", 'cluster-init')
+
+def recreate_names(font):
+    font.copyright = COPYRIGHT
+    font.familyname = FONT_NAME
+    font.fontname = re.sub(r"[^!-$&'*-.0-;=?-Z\\^-z|~]+", '', FONT_NAME)
+    font.fullname = FONT_NAME
+    font.sfntRevision = None
+    font.version = VERSION
+    font.sfnt_names = (
+            (b'English (US)', b'SubFamily', 'Regular'),
+            (b'English (US)', b'UniqueID', '{}:{}'.format(font.fullname, font.version)),
+            (b'English (US)', b'License', LICENSE),
+            (b'English (US)', b'License URL', 'http://scripts.sil.org/OFL'),
+        )
 
 def kss1init(font, nominal_glyph):
     glyph = font.createChar(-1, nominal_glyph.glyphname + '.kss1init')
@@ -123,6 +237,7 @@ def augment(font):
     cgj(font)
     zwsp(font)
     add_lookups(font)
+    recreate_names(font)
     font.hhea_linegap = int((MAX_EXTRA_ROWS - (1 - CLUSTER_Y_SCALE)) * font.ascent)
     glyph_names = []
     for glyph in font.selection.select((b'ranges', b'unicode'), 0xE000, 0xF8FF).byGlyphs:
